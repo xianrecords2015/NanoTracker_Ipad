73272a252193fa0ee4720cd30fac7a4d717c4c6b
Christian Joly <christianjoly@christians-mac-mini.local>
2015-10-29 18:03:27 -0700

    Initial Commit

---
+++ NanoTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NanoTracker
+//
+//  Created by Christian Joly on 10/29/15.
+//  Copyright © 2015 Christian Joly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NanoTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NanoTracker
+//
+//  Created by Christian Joly on 10/29/15.
+//  Copyright © 2015 Christian Joly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
